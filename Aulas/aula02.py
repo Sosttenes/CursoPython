59033c3c3a50c582bf4f999a37ff2a1d0716f728
Sosttenes <esantos@acad.ifma.edu.br>
2025-03-13 19:41:52 -0300

    Add Aula
    Aula sobre módulos

---
+++ Aulas/aula02.py
@@ -0,0 +1,7 @@
+# Utilização de módulos
+# Módulos são arquivos que contêm funções, classes e variáveis que podem ser importadas para outros arquivos.
+# Exemplo:
+import math # Importa o módulo math
+n = int(input('Digite um número: '))
+r = math.sqrt(n) # Calcula a raiz quadrada de n
+print(f'A raiz quadrada de {n} é {r}')
